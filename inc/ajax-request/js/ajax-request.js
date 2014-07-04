6e0b528c2b1ff199c1fd935c8cb88f21224a69dc
Matias Saggiorato <matiassaggiorato@gmail.com>
2014-07-03 23:44:31 -0300

    * Added getJSON and get methods to the ajax request module. These work the same way as post, with the same interface for the methods.

--- inc/ajax-request/js/ajax-request.js
+++ inc/ajax-request/js/ajax-request.js
@@ -21,4 +21,47 @@
 			}
 		})
 	}
+	window.ajaxGetJSON = function(action, _data, callback) {
+		if( typeof _data === "function"){
+			callback = _data;
+			_data = {};
+		}
+		if ( typeof _data !== "string" ) {
+			_data = jQuery.param( _data, false );
+		}
+		return jQuery.ajax(ajax_url, {
+			dataType: "json",
+			data: "action="+action+"&"+_data,
+			complete: function(jqXHR){
+				var json;
+				try{
+					json = $.parseJSON(jqXHR.responseText);
+				} catch (e) {
+					json = jqXHR.responseText;
+				}
+				callback.call(this, jqXHR.responseText, json);
+			}
+		});
+	}
+	window.ajaxGet = function(action, _data, callback) {
+		if( typeof _data === "function"){
+			callback = _data;
+			_data = {};
+		}
+		if ( typeof _data !== "string" ) {
+			_data = jQuery.param( _data, false );
+		}
+		return jQuery.ajax(ajax_url, {
+			data: "action="+action+"&"+_data,
+			complete: function(jqXHR){
+				var json;
+				try{
+					json = $.parseJSON(jqXHR.responseText);
+				} catch (e) {
+					json = jqXHR.responseText;
+				}
+				callback.call(this, jqXHR.responseText, json);
+			}
+		});
+	}
 })(jQuery)
