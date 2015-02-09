68fff2f47c160bf64261ed9341610401695bb524
Matias Saggiorato <matiassaggiorato@gmail.com>
2015-02-09 13:42:07 -0300

    Updated Bootstrap 3 to the latest version

--- inc/bootstrap3/bootstrap3.php
+++ inc/bootstrap3/bootstrap3.php
@@ -1,10 +1,10 @@
 <?php
 
 add_action( "init", function(){
-    wp_register_script( SAUCAL_TPL_ID."-bootstrap3", SAUCAL_TPL_LIB_URL(__FILE__)."/js/bootstrap.min.js", array(), "3.1.1", false );
+    wp_register_script( SAUCAL_TPL_ID."-bootstrap3", SAUCAL_TPL_LIB_URL(__FILE__)."/js/bootstrap.min.js", array(), "3.3.2", false );
 
-    wp_register_style( SAUCAL_TPL_ID."-bootstrap3", SAUCAL_TPL_LIB_URL(__FILE__)."/css/bootstrap.min.css", array(), "3.1.1" );
-    wp_register_style( SAUCAL_TPL_ID."-bootstrap3-theme", SAUCAL_TPL_LIB_URL(__FILE__)."/css/bootstrap-theme.min.css", array(SAUCAL_TPL_ID."-bootstrap3"), "3.1.1" );
+    wp_register_style( SAUCAL_TPL_ID."-bootstrap3", SAUCAL_TPL_LIB_URL(__FILE__)."/css/bootstrap.min.css", array(), "3.3.2" );
+    wp_register_style( SAUCAL_TPL_ID."-bootstrap3-theme", SAUCAL_TPL_LIB_URL(__FILE__)."/css/bootstrap-theme.min.css", array(SAUCAL_TPL_ID."-bootstrap3"), "3.3.2" );
 });
 add_action( "wp_enqueue_scripts", function(){
     wp_enqueue_style( SAUCAL_TPL_ID."-bootstrap3" );
