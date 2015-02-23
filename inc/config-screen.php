bd2d57673aeda631ed4eeb792831b09029515a27
Matias Saggiorato <matiassaggiorato@gmail.com>
2015-02-23 14:29:52 -0300

    Some improvements and changes to the config screen settings class.
    I've cleaned up all references to our internal POS project.

--- inc/config-screen.php
+++ inc/config-screen.php
@@ -1,32 +1,35 @@
 <?php
-class POSSettings {
+class Settings_Screen {
 	var $opts = array(); 
-    function POSSettings( $opts ) {
+    function Settings_Screen( $opts ) {
     	$this->opts = array_merge(array(
     		"title" => "Settings Page",
-    		"settings" => array()
+    		"settings" => array(),
+    		"slug" => "settings",
     	), $opts);
         add_filter( 'admin_init' , array( &$this , 'register_fields' ) );
-        add_action( 'admin_menu', array( &$this , 'pos_settings_menu' ) );
+        add_action( 'admin_menu', array( &$this , 'settings_menu' ) );
     }
     function register_fields() {
-	    add_settings_section( 'pos-general', '', array(&$this, 'section_one_callback'), 'pos-settings' );
-
-	    foreach ($this->opts["settings"] as $meta => $settingOpts) {
-	    	$settingOpts = array_merge(array(
-	    		"title" => "Setting",
-	    		"save_function" => "esc_attr",
-	    		"get_function" => false
-	    	), $settingOpts);
-	    	$this->add_setting($meta, $settingOpts["title"], "pos-general", $settingOpts["save_function"], $settingOpts["get_function"]);
-	    };
+	    foreach ($this->opts["settings"] as $section_slug => $section) {
+	    	$section_slug = $this->opts["slug"].'-'.$section_slug;
+	    	add_settings_section($section_slug , '', array(&$this, 'section_one_callback'), $this->opts["slug"] );
+		    foreach ($section as $meta => $settingOpts) {
+		    	$this->add_setting($meta, $section_slug, $settingOpts);
+		    };
+		};
     }
-    function add_setting($meta, $title, $section, $saveClear = "esc_attr", $getFunction = false){
-    	register_setting( 'pos-settings-group', $meta, $saveClear );
-        add_settings_field($meta, '<label for="'.$meta.'">'.$title.'</label>' , array(&$this, 'fields_html') , 'pos-settings', $section, array(
-        	"meta" => $meta,
-        	"get_function" => $getFunction
-        ) );
+    function add_setting($meta, $section, $options){
+    	$options = array_merge(array(
+    		"title" => "Setting",
+    		"save_function" => "esc_attr",
+    		"get_function" => false,
+    		"type" => "text",
+    		"meta" => $meta
+    	), $options);
+    	extract($options);
+    	register_setting( $this->opts["slug"].'-group', $meta, $save_function );
+        add_settings_field($meta, '<label for="'.$meta.'">'.$title.'</label>' , array(&$this, 'fields_html') , $this->opts["slug"], $section, $options );
     }
     function section_one_callback() {
 	    //echo 'Some help text goes here.';
@@ -38,19 +41,29 @@ class POSSettings {
     	} else {
     		$value = get_option( $meta, '' );
     	}
+    	$type = $args["type"];
 
-        echo '<input class="regular-text" type="text" id="'.$meta.'" name="'.$meta.'" value="' . $value . '" />';
+    	switch ($type) {
+    		case 'textarea':
+    		case 'paragraph':
+        		echo '<textarea class="widefat" id="'.$meta.'" name="'.$meta.'" rows="6" style="resize: vertical;">'.$value.'</textarea>';
+    			break;
+    		
+    		default:
+        		echo '<input class="widefat" type="text" id="'.$meta.'" name="'.$meta.'" value="' . $value . '" />';
+    			break;
+    	}
     }
-	function pos_settings_menu() {
-	    add_options_page( $this->opts["title"], $this->opts["title"], 'manage_options', 'pos-settings', array( &$this , 'pos_settings_page' ) );
+	function settings_menu() {
+	    add_options_page( $this->opts["title"], $this->opts["title"], 'manage_options', $this->opts["slug"], array( &$this , 'settings_page' ) );
 	}
-	function pos_settings_page() {
+	function settings_page() {
 	    ?>
 	    <div class="wrap">
 	        <h2><?= $this->opts["title"]; ?></h2>
 	        <form action="options.php" method="POST">
-	            <?php settings_fields( 'pos-settings-group' ); ?>
-	            <?php do_settings_sections( 'pos-settings' ); ?>
+	            <?php settings_fields( $this->opts["slug"].'-group' ); ?>
+	            <?php do_settings_sections( $this->opts["slug"] ); ?>
 	            <?php submit_button(); ?>
 	        </form>
 	    </div>
