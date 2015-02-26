1fda2e337154b7749a2f276ee965ecb4a5bd3a02
Matias Saggiorato <matiassaggiorato@gmail.com>
2015-02-26 19:30:55 -0300

    Updated perfect-scrollbar library to it's latest release, and made some improvements.
    Most improvements are related to using the body as the scrollable element.

--- inc/scrollbars/js/more-perfect-scrollbar.js
+++ inc/scrollbars/js/more-perfect-scrollbar.js
@@ -6,16 +6,31 @@
 
 			$(this).data("morePerfectScrollbarInitialized", true);
 
-			var thisArea = $(this).css({
+			var thisArea = $(this);
+			var thisAreaReal = thisArea;
+			var thisAreaScrollEvent = thisArea;
+
+			if(thisArea.is("body")){
+				thisArea.css({
+					"height": "100%",
+					"position": "relative"
+				});
+				thisArea.parent().css("height", "100%");
+				thisArea = thisArea.wrapInner('<div class="body-scroll-helper"></div>').children(".body-scroll-helper").css({
+					"position": "absolute",
+					"height": "100%",
+					"width": "100%",
+					"left": 0,
+					"top": 0
+				});
+				thisAreaScrollEvent = $(window);
+			}
+
+			thisArea.css({
 				overflow: "hidden"
 			});
 			if(thisArea.css("position") == "static"){
 				thisArea.css("position", "relative");
-			}
-
-			if(thisArea.is("body")){
-				thisArea.css("height", "100%");
-				thisArea.parent().css("height", "100%");
 			}
 
 			var helper = thisArea.children(".ps-scroll-helper");
@@ -32,15 +47,80 @@
 			});
 
 			//elements appended to this scrollable will now go to the helper element and refresh the scrollbar
-			var c = thisArea.get(0);
+			var c = thisAreaReal.get(0);
+			var helperElement = helper.get(0);
+
+			var patchAppendChild = function(el) {
+				if(el.appendChild) {
+					var oldAppendChild = el.appendChild;
+					el.appendChild = function(){
+						patchAppendChild(arguments[0]);
+						var ret = oldAppendChild.apply(this, arguments);
+						thisArea.perfectScrollbar("update");
+						return ret;
+					}
+				}
+				if(el.removeChild) {
+					var oldRemoveChild = el.removeChild;
+					el.removeChild = function(){
+						var ret = oldRemoveChild.apply(this, arguments);
+						thisArea.perfectScrollbar("update");
+						return ret;
+					}
+				}
+			}
+
 			c.__appendChild = c.appendChild;
 			c.appendChild = function(){
-			     c.__appendChild.apply(helper.get(0), arguments); 
-			     thisArea.perfectScrollbar("update");
+				var ret = c.__appendChild.apply(helperElement, arguments);
+				thisArea.perfectScrollbar("update");
+				return ret;
 			}
+			c.__removeChild = c.removeChild;
+			c.removeChild = function(){
+				var ret = c.__removeChild.apply(helperElement, arguments);
+				thisArea.perfectScrollbar("update");
+				return ret;
+			}
+			patchAppendChild(c)
+
+			var d = thisAreaScrollEvent.get(0);
+			d.__pageYOffset = d.pageYOffset;
+			Object.defineProperty(d, 'pageYOffset', {
+			  get: function() { return thisArea.scrollTop(); },
+			  set: function(newValue) { thisArea.scrollTop(newValue); }
+			});
+			d.__pageXOffset = d.pageXOffset;
+			Object.defineProperty(d, 'pageXOffset', {
+			  get: function() { return thisArea.scrollLeft(); },
+			  set: function(newValue) { thisArea.scrollLeft(newValue); }
+			});
+
+			if(d.scrollTo || d.scroll) {
+				//bypass scroll settings to the actual scrolling area
+				d.__scrollTo = d.scrollTo;
+				d.__scroll = d.scroll;
+				var prevScrollX = thisArea.scrollLeft();
+				var prevScrollY = thisArea.scrollTop(); 
+				d.scroll = d.scrollTo = function() {
+					var thisAreaElem = thisArea.get(0);
+					if(prevScrollX != arguments[0]) {
+						thisArea.scrollLeft(arguments[0]);
+					}
+					if(prevScrollY != arguments[1]){
+						thisArea.scrollTop(arguments[1]);
+					}
+					prevScrollX = thisArea.scrollLeft();
+					prevScrollY = thisArea.scrollTop(); 
+					d.__scroll.apply(this, arguments);
+					return d;
+				}
+			}
+
+			thisArea.on("scroll", function(){
+				$(window).trigger("scroll");
+			})
 		});
 	}
-	$(function(){
-		$(".scrollable").morePerfectScrollbar();
-	});
+	$(".scrollable").morePerfectScrollbar();
 })(jQuery)
--- inc/scrollbars/scrollbars.php
+++ inc/scrollbars/scrollbars.php
@@ -2,10 +2,10 @@
 add_action( "init", function(){
     wp_register_style( SAUCAL_TPL_ID."-perfect-scrollbar", SAUCAL_TPL_LIB_URL(__FILE__)."/css/perfect-scrollbar.css", array(), "0.5.8", false );
     wp_register_script( SAUCAL_TPL_ID."-perfect-scrollbar", SAUCAL_TPL_LIB_URL(__FILE__)."/js/perfect-scrollbar.js", array("jquery"), "0.5.8", false );
-    wp_register_script( SAUCAL_TPL_ID."-more-perfect-scrollbar", SAUCAL_TPL_LIB_URL(__FILE__)."/js/more-perfect-scrollbar.js", array(SAUCAL_TPL_ID."-perfect-scrollbar", "saucal-jquery-extension"), "0.1", false );
+    wp_register_script( SAUCAL_TPL_ID."-more-perfect-scrollbar", SAUCAL_TPL_LIB_URL(__FILE__)."/js/more-perfect-scrollbar.js", array(SAUCAL_TPL_ID."-perfect-scrollbar", "saucal-jquery-extension"), "0.1", true );
 });
 add_action( "wp_enqueue_scripts", function(){
     wp_enqueue_script( SAUCAL_TPL_ID."-more-perfect-scrollbar" );
     wp_enqueue_style( SAUCAL_TPL_ID."-perfect-scrollbar" );
-});
+}, 1);
 ?>
