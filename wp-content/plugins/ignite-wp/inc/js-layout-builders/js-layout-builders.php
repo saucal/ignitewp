32666c9418437620ef8f6fcab6aa94cf980a2678
Matias Saggiorato <matiassaggiorato@gmail.com>
2015-03-16 20:46:16 -0300

    Updated js-layout-builders library to the new standard.

--- wp-content/plugins/ignite-wp/inc/js-layout-builders/js-layout-builders.php
+++ wp-content/plugins/ignite-wp/inc/js-layout-builders/js-layout-builders.php
@@ -1,8 +1,3 @@
 <?php
-add_action( "init", function(){
-    wp_register_script( SAUCAL_TPL_ID."-layout-builders", SAUCAL_TPL_LIB_URL(__FILE__)."/js/layout-builders.js", array("jquery"), "1.0", false );
-});
-add_action( "wp_enqueue_scripts", function(){
-    wp_enqueue_script( SAUCAL_TPL_ID."-layout-builders" );
-}, 5);
+ignite_register_script("layout-builders", "js/layout-builders.js", array("jquery"), "1.0", false, 5 );
 ?>
