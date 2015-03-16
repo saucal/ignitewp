d67efd125ea3bdf08eb8a7f33c87bd4f2c778d4c
Matias Saggiorato <matiassaggiorato@gmail.com>
2015-03-16 17:25:20 -0300

    Added support to load a module conditionally on admin, on front, or on both enviroments.
    This turn out to be useful to use some of the frontend functionality in both the admin, and the front end.

--- wp-content/plugins/ignite-wp/inc/core.php
+++ wp-content/plugins/ignite-wp/inc/core.php
@@ -1,5 +1,8 @@
 <?php 
-function add_ignite_support($modules) {
+global $moduleEnvUrl, $enqueueEnviroment;
+$moduleEnvUrl = array();
+$enqueueEnviroment = array();
+function add_ignite_support($modules, $enviroment = "front") {
     if(!is_array($modules))
         $modules = array($modules);
 
@@ -36,9 +39,156 @@ function add_ignite_support($modules) {
         }
         
         add_theme_support("ignite-".$module);
+
+        global $moduleEnvUrl, $enqueueEnviroment;
+        $moduleEnvUrl[] = SAUCAL_TPL_LIB_URL($fullPath);
+        $enqueueEnviroment[] = $enviroment;
+
         require_once($fullPath);
+
+        array_pop($moduleEnvUrl);
+        array_pop($enqueueEnviroment);
     }
 }
+
+function ignite_get_current_enviroment(){
+    global $moduleEnvUrl, $enqueueEnviroment;
+    return array(
+        "moduleEnvUrl" => count($moduleEnvUrl) ? end($moduleEnvUrl) : null,
+        "enqueueEnviroment" => count($enqueueEnviroment) ? end($enqueueEnviroment) : null
+    );
+}
+
+class Ignite_Scripts {
+    var $scripts = array();
+    function Ignite_Scripts() {
+        add_action("wp", array($this, "initialize_registering"));
+    }
+    function register_script($script_id, $path, $deps = array(), $ver = false, $in_footer = false, $enqueue_at_priority = "no") {
+        extract(ignite_get_current_enviroment());
+
+        if(!isset($enqueueEnviroment))
+            $enqueueEnviroment = "front";
+
+        if(!isset($this->scripts[$enqueueEnviroment])) 
+            $this->scripts[$enqueueEnviroment] = array();
+        
+        if(!isset($this->scripts[$enqueueEnviroment][$enqueue_at_priority])) 
+            $this->scripts[$enqueueEnviroment][$enqueue_at_priority] = array();
+
+        $this->scripts[$enqueueEnviroment][$enqueue_at_priority][] = array($script_id, rtrim($moduleEnvUrl, "/")."/".$path, $deps, $ver, $in_footer);
+    }
+    function initialize_registering(){
+        extract(ignite_get_current_enviroment());
+
+        foreach($this->scripts as $env => $prios){
+            foreach($prios as $prio => $scripts) {
+                foreach($scripts as $script){
+                    call_user_func_array("wp_register_script", $script);
+                }
+                if(is_numeric($prio)) {
+                    $cbs = array();
+                    switch ($env) {
+                        case 'front':
+                            $cbs[] = "wp_enqueue_scripts";
+                            break;
+                        case 'admin':
+                            $cbs[] = "admin_enqueue_scripts";
+                            break;
+                        case 'both':
+                            $cbs[] = "wp_enqueue_scripts";
+                            $cbs[] = "admin_enqueue_scripts";
+                            break;
+                    }
+                    foreach($cbs as $cb) {
+                        add_action($cb, create_function("", "return IgniteScripts()->enqueue_list(".var_export($scripts, true).");"), $prio);
+                    }
+                }
+            }
+        }
+    }
+    function enqueue_list($list) {
+        foreach($list as $script) {
+            wp_enqueue_script($script[0]);
+        }
+    }
+}
+function ignite_register_script($script_id, $path, $deps = array(), $ver = false, $in_footer = false, $enqueue_at_priority = "no") {
+    return IgniteScripts()->register_script($script_id, $path, $deps, $ver, $in_footer, $enqueue_at_priority);
+}
+function IgniteScripts(){
+    static $instance;
+    if(!isset($instance))
+        $instance = new Ignite_Scripts();
+    return $instance;
+}
+IgniteScripts();
+
+class Ignite_Styles {
+    var $scripts = array();
+    function Ignite_Styles() {
+        add_action("wp", array($this, "initialize_registering"));
+    }
+    function register_style($script_id, $path, $deps = array(), $ver = false, $media = "all", $enqueue_at_priority = "no") {
+        extract(ignite_get_current_enviroment());
+
+        if(!isset($enqueueEnviroment))
+            $enqueueEnviroment = "front";
+
+        if(!isset($this->scripts[$enqueueEnviroment])) 
+            $this->scripts[$enqueueEnviroment] = array();
+        
+        if(!isset($this->scripts[$enqueueEnviroment][$enqueue_at_priority])) 
+            $this->scripts[$enqueueEnviroment][$enqueue_at_priority] = array();
+
+        $this->scripts[$enqueueEnviroment][$enqueue_at_priority][] = array($script_id, rtrim($moduleEnvUrl, "/")."/".$path, $deps, $ver, $media);
+    }
+    function initialize_registering(){
+        extract(ignite_get_current_enviroment());
+
+        foreach($this->scripts as $env => $prios){
+            foreach($prios as $prio => $scripts) {
+                foreach($scripts as $script){
+                    call_user_func_array("wp_register_style", $script);
+                }
+                if(is_numeric($prio)) {
+                    $cbs = array();
+                    switch ($env) {
+                        case 'front':
+                            $cbs[] = "wp_enqueue_scripts";
+                            break;
+                        case 'admin':
+                            $cbs[] = "admin_enqueue_scripts";
+                            break;
+                        case 'both':
+                            $cbs[] = "wp_enqueue_scripts";
+                            $cbs[] = "admin_enqueue_scripts";
+                            break;
+                    }
+                    foreach($cbs as $cb) {
+                        add_action($cb, create_function("", "return IgniteStyles()->enqueue_list(".var_export($scripts, true).");"), $prio);
+                    }
+                }
+            }
+        }
+    }
+    function enqueue_list($list) {
+        foreach($list as $script) {
+            wp_enqueue_style($script[0]);
+        }
+    }
+}
+function ignite_register_style($script_id, $path, $deps = array(), $ver = false, $media = "all", $enqueue_at_priority = "no") {
+    return IgniteStyles()->register_style($script_id, $path, $deps, $ver, $media, $enqueue_at_priority);
+}
+function IgniteStyles(){
+    static $instance;
+    if(!isset($instance))
+        $instance = new Ignite_Styles();
+    return $instance;
+}
+IgniteStyles();
+
 
 function SAUCAL_TPL_LIB_DIR($file) {
 	return realpath(dirname($file));
