ccc9a4196b6f310f87dab0b90bf637dbe15efd1e
Matias Saggiorato <matiassaggiorato@gmail.com>
2015-03-20 04:15:04 -0300

    Allow bootstrap3 to be replaced from the theme.
    This will allow us to include a recompiled version with a custom grid.

--- wp-content/plugins/ignite-wp/inc/bootstrap3/bootstrap3.php
+++ wp-content/plugins/ignite-wp/inc/bootstrap3/bootstrap3.php
@@ -1,10 +1,16 @@
 <?php
-
 add_action( "init", function(){
-    wp_register_script( SAUCAL_TPL_ID."-bootstrap3", SAUCAL_TPL_LIB_URL(__FILE__)."/js/bootstrap.min.js", array(), "3.3.2", false );
+	$url = SAUCAL_TPL_LIB_URL(__FILE__);
+	$themeDir = SAUCAL_TPL_BASE."/inc/bootstrap3";
+	$ver = "3.3.2";
+	if(file_exists($themeDir) && is_dir($themeDir)) {
+		$url = SAUCAL_TPL_BASEURL."/inc/bootstrap3";
+		$ver = "3.x.custom";
+	}
+    wp_register_script( SAUCAL_TPL_ID."-bootstrap3", $url."/js/bootstrap.min.js", array(), $ver, false );
 
-    wp_register_style( SAUCAL_TPL_ID."-bootstrap3", SAUCAL_TPL_LIB_URL(__FILE__)."/css/bootstrap.min.css", array(), "3.3.2" );
-    wp_register_style( SAUCAL_TPL_ID."-bootstrap3-theme", SAUCAL_TPL_LIB_URL(__FILE__)."/css/bootstrap-theme.min.css", array(SAUCAL_TPL_ID."-bootstrap3"), "3.3.2" );
+    wp_register_style( SAUCAL_TPL_ID."-bootstrap3", $url."/css/bootstrap.min.css", array(), $ver );
+    wp_register_style( SAUCAL_TPL_ID."-bootstrap3-theme", $url."/css/bootstrap-theme.min.css", array(SAUCAL_TPL_ID."-bootstrap3"), $ver );
 });
 add_action( "wp_enqueue_scripts", function(){
     wp_enqueue_style( SAUCAL_TPL_ID."-bootstrap3" );
