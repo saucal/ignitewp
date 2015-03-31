985de7e90aed85cc5fefd8fd9f87ae9789eb37dd
Matias Saggiorato <matiassaggiorato@gmail.com>
2015-03-31 20:16:33 -0300

    Load all scripts and styles for all forms in every page, it's needed for AJAX navigation.
    This should probably be included in a separate module, and be required by the AJAX module.

--- wp-content/plugins/ignite-wp/inc/gravity-forms-css-reset/gravity-forms-css-reset.php
+++ wp-content/plugins/ignite-wp/inc/gravity-forms-css-reset/gravity-forms-css-reset.php
@@ -1,2 +1,16 @@
+<?php
 ignite_register_style("gravity-forms-css-reset", "css/gravity-forms-css-reset.css", array(), "1.0", false, 100 );
 ignite_register_script("gravity-forms-css-reset", "js/gravity-forms-css-reset.js", array("jquery"), "1.0", false, 100 );
+
+add_action("wp_enqueue_scripts", function(){
+	$forms = RGFormsModel::get_forms( null, 'title' );
+	foreach ( $forms as $_form ) {
+		$form = RGFormsModel::get_form_meta( $_form->id );
+		if ( isset( $form['id'] ) ) {
+			require_once( GFCommon::get_base_path() . '/form_display.php' );
+			GFFormDisplay::enqueue_form_scripts( $form, true );
+		}
+	}
+});
+
+?>
