3d242624f0307f6bcf110a99ba5cbdd8f578a191
Matias Saggiorato <matiassaggiorato@gmail.com>
2015-03-31 20:15:22 -0300

    Replace the stock submit button with a button tag (it used an input tag)

--- wp-content/plugins/ignite-wp/inc/gravity-forms-css-reset/gravity-forms-css-reset.php
+++ wp-content/plugins/ignite-wp/inc/gravity-forms-css-reset/gravity-forms-css-reset.php
@@ -1,1 +1,2 @@
 ignite_register_style("gravity-forms-css-reset", "css/gravity-forms-css-reset.css", array(), "1.0", false, 100 );
+ignite_register_script("gravity-forms-css-reset", "js/gravity-forms-css-reset.js", array("jquery"), "1.0", false, 100 );
---
+++ wp-content/plugins/ignite-wp/inc/gravity-forms-css-reset/js/gravity-forms-css-reset.js
@@ -0,0 +1,18 @@
+(function($){
+	$(document).on("gform_page_loaded gform_post_render", function(e, form_id){
+		var scope = $("#gform_wrapper_"+form_id);
+		scope.find(".gform_footer input.button, .gform_footer input[type=submit]").each(function(){
+			var thisBtn = $(this);
+			var thisForm = thisBtn.parents(".gform_wrapper");
+
+			var newBtn = $("<button/>", {
+				"type": "submit",
+				"id": thisBtn.attr("id"),
+				"class": thisBtn.attr("class"),
+				"tabindex": thisBtn.attr("tabindex"),
+				"onclick": thisBtn.attr("onclick")
+			}).text(thisBtn.attr("value")).insertAfter(thisBtn);
+			thisBtn.detach();
+		})
+	})
+})(jQuery)
