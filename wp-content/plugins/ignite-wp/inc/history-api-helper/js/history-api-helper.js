06c561911edf42566669df927a066d85d056ca54
Matias Saggiorato <matiassaggiorato@gmail.com>
2015-06-18 22:13:38 -0300

    Improved History API helper to not trigger the pushState event when a popstate is ocurring.

--- wp-content/plugins/ignite-wp/inc/history-api-helper/js/history-api-helper.js
+++ wp-content/plugins/ignite-wp/inc/history-api-helper/js/history-api-helper.js
@@ -12,6 +12,9 @@
 	    this.off = $.proxy(this.evtElem.off, this.evtElem);
 	    this.trigger = $.proxy(this.evtElem.trigger, this.evtElem);
 
+	    this.popping = false;
+	    var self = this;
+
 		$(window).bind('popstate', function(event){
 	        // Ignore inital popstate that some browsers fire on page load
 	        var initialPop = !historyAPI.popped && location.href == historyAPI.initialURL;
@@ -35,15 +38,42 @@
 	        	}
 	        }
 
+	        self.popping = true;
 	        historyAPI.trigger(evt, fixed);
+	        self.popping = false;
 	        if(!evt.isDefaultPrevented() && fixed[1] !== null) {
 	        	historyAPI.doPopState.apply(historyAPI, fixed);
 	        }
 	    });
 	}
 
+	var getLocationObject = function(url) {
+		var a = document.createElement('a');
+	    a.href = url;
+	    return {
+	    	hash: a.hash,
+	    	host: a.host,
+	    	hostname: a.hostname,
+	    	href: a.href,
+	    	pathname: a.pathname,
+	    	port: a.port,
+	    	protocol: a.protocol,
+	    	search: a.search
+	    }
+	}
+
+	var HTMLtoText = function(html) {
+		var elem = $('<textarea />');
+		var varTitle = elem.html(html).text();
+		elem.remove();
+		return varTitle;
+	}
+
 	SAUCAL_HISTORY_API.prototype.pushState = function(state, title, href){
+		if(this.popping)
+			return;
     	var evt = $.Event("pushstate");
+    	title = HTMLtoText(title);
     	this.trigger(evt, [href]);
     	if(historyAPI.historyApiSupported() && !evt.isDefaultPrevented()){
     		history.pushState({
@@ -60,6 +90,7 @@
     }
     SAUCAL_HISTORY_API.prototype.replaceState = function(state, title, href){
     	var evt = $.Event("replacestate");
+    	title = HTMLtoText(title);
     	this.trigger(evt, [href]);
     	if(historyAPI.historyApiSupported() && !evt.isDefaultPrevented()){
     		history.replaceState({
@@ -74,6 +105,7 @@
     	return evt;
     }
     SAUCAL_HISTORY_API.prototype.doPopState = function(state, title, href) {
+    	title = HTMLtoText(title);
     	if(historyAPI.historyApiSupported()){
     		document.title = title;
     		this.analyticsPush(title, href);
