289cbef0465f7786d8dd3bd0a86e3353ab5613ef
Matias Saggiorato <matiassaggiorato@gmail.com>
2015-11-13 16:00:44 -0300

    Added imagesloaded module

---
+++ wp-content/plugins/ignite-wp/inc/js-imagesloaded.php
@@ -0,0 +1,3 @@
+<?php 
+ignite_register_script("jquery-imagesloaded", "//cdnjs.cloudflare.com/ajax/libs/jquery.imagesloaded/3.1.8/imagesloaded.pkgd.min.js", array("jquery"), "3.1.8", false, 5 );
+?>
